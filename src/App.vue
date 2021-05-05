3d69e1327d32aebcea877f78f54eff53b5cbd4c0
wintyo <wintyo1024@gmail.com>
2021-05-06 07:40:44 +0900

    Viteプロジェクトの初期設定

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  p Hello, World
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3200,
+  },
+  resolve: {
+    alias: {
+      // 上手くaliasの設定ができていない
+      '~/': path.resolve(__dirname, './src/'),
+    },
+  },
+  plugins: [vue()],
+});
