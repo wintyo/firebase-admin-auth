69b2db5a01920c09037a15dad9ead4542a2b0f5b
wintyo <wintyo1024@gmail.com>
2021-05-06 08:40:12 +0900

    画像認証の実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,25 @@
 import express from 'express';
+import cookie from 'cookie';
 import cors from 'cors';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+/** content-typeが分からなかったときのデフォルト */
+const DEFAULT_CONTENT_TYPE = 'image/png';
+
+/** 拡張子からcontent-typeを算出するオブジェクトマップ */
+const EXT_NAME2CONTENT_TYPE_MAP: Record<string, string> = {
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  gif: 'image/gif',
+  bmp: 'image/bmp',
+};
+
+/** セッション情報をCookieに保存するキー名 */
+const SESSION_COOKIE_KEY = '__session';
+
+admin.initializeApp();
 
 const app = express();
 app.use(express.json());
@@ -14,10 +33,119 @@ app.use(
   })
 );
 
+function addCookie(res: any, key: string, value: string) {
+  const expiresIn = 60 * 60 * 24;
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize(key, value, {
+      maxAge: expiresIn,
+      httpOnly: true,
+    })
+  );
+}
+
 const apiRouter = express.Router();
 
-apiRouter.get('/health', (req, res) => {
-  res.send('I am OK!');
+apiRouter
+  .get('/cookies', (req, res) => {
+    res.setHeader('Cache-Control', 'private');
+    const cookies = cookie.parse(req.headers.cookie || '');
+    res.send(cookies);
+  })
+  .post('/cookie', (req, res) => {
+    res.setHeader('Cache-Control', 'private');
+    addCookie(res, req.body.key, req.body.value);
+    res.send('save cookies');
+  });
+
+apiRouter.post('/auth', async (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.body.idToken)
+    .then((decodedIdToken) => {
+      // 認証してから5分以上経過しているトークンはエラーにする
+      if (Date.now() / 1000 - decodedIdToken.auth_time >= 5 * 60) {
+        throw new Error('auth expired');
+      }
+      return admin.auth().createSessionCookie(req.body.idToken, {
+        expiresIn: 24 * 60 * 60 * 1000,
+      });
+    })
+    .then((sessionCookie) => {
+      addCookie(res, SESSION_COOKIE_KEY, sessionCookie);
+      res.send('ok');
+    })
+    .catch(() => {
+      res.status(401).send('auth error');
+    });
+});
+
+apiRouter.get('/profile', async (req, res) => {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const sessionCookie = cookies[SESSION_COOKIE_KEY] || '';
+
+  try {
+    const decodedClaims = await admin.auth().verifySessionCookie(sessionCookie, true);
+    const user = await admin.auth().getUser(decodedClaims.uid);
+    res.send(user);
+  } catch {
+    res.status(401).send('auth error');
+  }
+});
+
+apiRouter.get('/image/:uid/:fileName', async (req, res) => {
+  const { uid, fileName } = req.params;
+  const bucket = admin.storage().bucket();
+
+  // no-image画像を返す
+  const responseNoImage = async (statusCode: number) => {
+    const file = bucket.file('public/images/no-image.jpg');
+    const data = await file.download();
+    res.status(statusCode);
+    res.contentType('image/jpeg');
+    res.end(data[0], 'binary');
+  };
+
+  try {
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const sessionCookie = cookies[SESSION_COOKIE_KEY] || '';
+
+    // 認証チェック
+    const decodedClaims = await admin.auth().verifySessionCookie(sessionCookie, true);
+
+    // 認証しているユーザとアクセスする画像があっていないときはエラー
+    if (decodedClaims.uid !== uid) {
+      throw new Error('user id is not matched');
+    }
+  } catch (err) {
+    // エラーの場合はno-image画像を返す
+    console.error(err);
+    await responseNoImage(401);
+    return;
+  }
+
+  // 画像データの取得
+  const imageData = await (async () => {
+    try {
+      const file = bucket.file(`images/${uid}/${fileName}`);
+      const data = await file.download();
+      return data[0];
+    } catch {
+      return null;
+    }
+  })();
+
+  // 画像データが存在しなかったらno-image画像を返す
+  if (imageData == null) {
+    console.log(`images/${uid}/${fileName} is not exist.`);
+    await responseNoImage(404);
+    return;
+  }
+
+  // 取得した画像データを返す
+  const [, ext] = fileName.split('.');
+  res.contentType(EXT_NAME2CONTENT_TYPE_MAP[ext] || DEFAULT_CONTENT_TYPE);
+  res.end(imageData, 'binary');
 });
 
 app.use('/api', apiRouter);
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,147 @@
 <template lang="pug">
 div
-  p Hello, World
+  template(v-if="state.isAuthorizing")
+    p 認証中
+  template(v-else)
+    template(v-if="state.user == null")
+      button(@click="onLoginButtonClick") ログイン
+    template(v-else)
+      p ログイン済
+      button(@click="onGetCookiesButtonClick") get cookies
+      div
+        div
+          label key:
+          input(v-model="cookieForm.key")
+        div
+          label value:
+          input(v-model="cookieForm.value")
+        button(@click="onPostCookiesButtonClick") post cookies
+      p
+        div {{ state.user.displayName }}
+        div {{ state.user.email }}
+      input(type="file", @change="onUploadFile")
+      p 画像一覧
+      template(v-for="imageInfo in state.imageInfoList")
+        div
+          img(:src="imageInfo.url")
+          div {{ imageInfo.name }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, onMounted } from 'vue';
+
+import firebase, { auth, authProviders, storage } from './firebase/';
+import axios from 'axios';
+
+interface IState {
+  isAuthorizing: boolean;
+  user: firebase.User | null;
+  imageInfoList: Array<{
+    name: string;
+    url: string;
+  }>;
+}
 
 export default defineComponent({
   name: 'App',
+  setup() {
+    const state = reactive<IState>({
+      isAuthorizing: true,
+      user: null,
+      imageInfoList: [],
+    });
+
+    const cookieForm = reactive<{ key: string; value: string }>({
+      key: '',
+      value: '',
+    });
+
+    onMounted(() => {
+      auth.onAuthStateChanged((user) => {
+        console.log(user);
+        state.isAuthorizing = false;
+        state.user = user;
+
+        if (state.user != null) {
+          const storageRef = storage.ref();
+          storageRef
+            .child(`images/${state.user.uid}`)
+            .listAll()
+            .then((value) => {
+              console.log(value);
+              // value.items
+              Promise.all(
+                value.items.map(async (item) => {
+                  const imageUrl: string = await item.getDownloadURL();
+                  return {
+                    name: item.name,
+                    url: imageUrl,
+                  };
+                })
+              ).then((imageInfoList) => {
+                console.log(imageInfoList);
+                state.imageInfoList = imageInfoList;
+              });
+            });
+
+          state.user.getIdToken().then(async (idToken) => {
+            console.log(idToken);
+
+            const res = await axios.post('/api/auth', {
+              idToken,
+            });
+            console.log(res);
+          });
+        }
+      });
+    });
+
+    return {
+      state,
+      cookieForm,
+      onGetCookiesButtonClick: async () => {
+        const res = await axios.get('/api/cookies');
+        console.log(res);
+      },
+      onPostCookiesButtonClick: async () => {
+        const res = await axios.post('/api/cookie', {
+          key: cookieForm.key,
+          value: cookieForm.value,
+        });
+        console.log(res);
+      },
+      onLoginButtonClick: () => {
+        auth.signInWithRedirect(authProviders.Google);
+      },
+      onUploadFile: (event: Event) => {
+        if (state.user == null) {
+          return;
+        }
+        const elInput = event.currentTarget as HTMLInputElement;
+        if (elInput.files == null) {
+          return;
+        }
+        const file = elInput.files[0];
+
+        const storageRef = storage.ref();
+        const uploadTask = storageRef.child(`images/${state.user.uid}/test.png`).put(file);
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            console.log(snapshot);
+          },
+          (err) => {
+            console.error(err);
+          },
+          () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadUrl) => {
+              console.log(downloadUrl);
+            });
+          }
+        );
+      },
+    };
+  },
 });
 </script>
 
